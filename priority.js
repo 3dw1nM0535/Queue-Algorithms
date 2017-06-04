72f453b7a09f7ffe075639aded561060f5a91ebf
3dw1nm0535 <workockmoses@gmail.com>
2017-06-04 21:08:40 +0300

    added test data

--- priority.js
+++ priority.js
@@ -18,7 +18,7 @@ function enqueue(element) {
 function dequeue(){
   var priority = this.dataStore[0].code;
   for (var i = 1; i < this.dataStore.length; i++) {
-    if(this.dataStore[i] < priority) {
+    if(this.dataStore[i].code < priority) {
       priority = i;
     }
   }
@@ -59,3 +59,28 @@ function Patient(name, code) {
   this.name = name;
   this.code = code;
 }
+
+//Adding test program
+var p = new Patient("Smith", 5);
+var ed = new Queue();
+ed.enqueue(p);
+p = new Patient("Jones", 4);
+ed.enqueue(p);
+p = new Patient("Audrey", 1);
+ed.enqueue(p);
+p = new Patient("Ingram", 3);
+ed.enqueue(p);
+p = new Patient("Brown", 2);
+ed.enqueue(p);
+
+console.log(ed.toString());
+
+var seen = ed.dequeue();
+console.log("Patient being treated: " + seen[0].name);
+console.log("Patient waiting to be treated: ");
+console.log(ed.toString());
+ //another round
+ var seen = ed.dequeue();
+ console.log("Patient being treated: " + seen[0].name);
+ console.log("Patient waiting to be treated: ");
+ console.log(ed.toString());
